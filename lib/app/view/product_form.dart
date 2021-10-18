123daf5078785335ba2a62651c0b6bc730e324b7
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2021-10-17 23:59:15 -0300

    finalizando CRUD

--- lib/app/view/product_form.dart
+++ lib/app/view/product_form.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:pdv_flutter/app/view/product_form_back.dart';
 
 class ProductForm extends StatelessWidget {
+  final _form = GlobalKey<FormState>();
 
-Widget fildName(){
+Widget fildName(ProductFormBack back){
   return TextFormField(
+    validator: back.validateName,
+    onSaved: (newValue) => back.product.nome = newValue,
+    initialValue: back.product.nome,
     decoration: InputDecoration(
       labelText: 'Nome'
     ),
   ); 
 }
 
-Widget fildQtd(){
+Widget fildQtd(ProductFormBack back){
   return TextFormField(
+    validator: back.validateQtd,
+    onSaved: (newValue) => back.product.quantidade = newValue,
+    initialValue: back.product.quantidade,
+    keyboardType: TextInputType.number,
     decoration: InputDecoration(
       labelText: 'Quantidade'
     ),
   ); 
 }
 
-Widget fildImg(){
+Widget fildImg(ProductFormBack back){
   return TextFormField(
+    onSaved: (newValue) => back.product.urlImg = newValue,
+    initialValue: back.product.urlImg,
     decoration: InputDecoration(
       labelText: 'Url Imagem',
       hintText: 'http://www.site.com'
@@ -29,22 +40,32 @@ Widget fildImg(){
 
   @override
   Widget build(BuildContext context) {
+    var _back = ProductFormBack(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Cadastro de Produto'),
         actions: [
-          IconButton(icon: Icon(Icons.save),
-          onPressed: null)
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: (){
+              _form.currentState.validate();
+              _form.currentState.save();
+              if(_back.isValid){
+                _back.save();
+                Navigator.of(context).pop();
+              }
+          })
         ],
       ),
       body: Padding(
         padding: EdgeInsets.all(10),
         child: Form(
+          key: _form,
           child: Column(
             children: [
-              fildName(),
-              fildQtd(),
-              fildImg()
+              fildName(_back),
+              fildQtd(_back),
+              fildImg(_back)
             ],
           )),
       ),
