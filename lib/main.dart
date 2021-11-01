e2d623211240cb15fe26896065d626b5873c1285
MarcioJCarvalho <marciojosedecarvalho@gmail.com>
2021-10-31 21:35:53 -0300

    adicionando o firebase e o firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:pdv_flutter/app/injection.dart';
 import 'app/app.dart';
-void main() {
+
+void main() async {
   setupeInjection();
   runApp(App());
+
+  await Firebase.initializeApp();
+  FirebaseFirestore.instance.collection('product').doc().set({
+    'name': 'Joice',
+    'phone': '(44) 9 9999-9999',
+    'e-mail': 'joice@email.com'
+  });
 }
